4a519c4f6d1f060e825f4b96858557e0a6ddf856
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2016-12-31 12:12:09 -0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DIRECTORY="~/.fortune_cow"
-
 if ! type "cowsay" &> /dev/null; then 
 	echo "Installing cowsay..."
 	python -mplatform | grep -qi Ubuntu && sudo apt install cowsay || sudo dnf install cowsay
@@ -16,16 +14,18 @@ else
 	echo "fortune is already installed."
 fi
 
-if [ ! -d "$DIRECTORY" ]; then
-	mkdir $DIRECTORY
+if [ ! -d "~/.fortune_cow/" ]; then
+	mkdir ~/.fortune_cow/
 fi
 
+touch ~/log
 echo "Downloading files..."
-wget -O ~/.fortune_cow/.check_day.sh "https://raw.githubusercontent.com/mateus558/Cow-of-Fortune/master/.check_day.sh" >> log
-wget -O ~/.fortune_cow/cow "https://raw.githubusercontent.com/mateus558/Cow-of-Fortune/master/cow" >> log
+wget -O ~/.fortune_cow/.check_day.sh "https://raw.githubusercontent.com/mateus558/Cow-of-Fortune/master/.check_day.sh" >> ~/log
+wget -O ~/.fortune_cow/cow "https://raw.githubusercontent.com/mateus558/Cow-of-Fortune/master/cow" >> ~/log
 echo "Installing script..."
-cat $DIRECTORY/cow >> $HOME/.bashrc
-rm $DIRECTORY/cow
+cat ~/.fortune_cow/cow >> $HOME/.bashrc
+rm ~/.fortune_cow/cow
 echo "Installing Sudocabulary..."
-wget -O ~/.vocab "https://goo.gl/N4EiTq" --no-check-certificate >> log
-wget -O ~/.vocabscript "https://goo.gl/gI7xKQ" --no-check-certificate >> log
+wget -O ~/.vocab "https://goo.gl/N4EiTq" --no-check-certificate >> ~/log
+wget -O ~/.vocabscript "https://goo.gl/gI7xKQ" --no-check-certificate >> ~/log
+rm ~/log
